14f9de6ffb2ea7bcecaaa09ea7509b63dfce46eb
saran <saranbs2013@gmail.com>
2016-09-04 13:39:52 +0800

    Fetched latest version from server

--- app/src/main/java/com/saran/cinebox/SplashActivity.java
+++ app/src/main/java/com/saran/cinebox/SplashActivity.java
@@ -1,13 +1,64 @@
 package com.saran.cinebox;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings firebaseRemoteConfigSettings =
+                new FirebaseRemoteConfigSettings.Builder()
+                        .setDeveloperModeEnabled(true)
+                        .build();
+
+        Map<String,Object>  defaultConfigMap = new HashMap<>();
+        defaultConfigMap.put("latest_version",0);
+        mFirebaseRemoteConfig.setConfigSettings(firebaseRemoteConfigSettings);
+        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
+        fetchConfig();
+    }
+
+    private void fetchConfig() {
+        long cacheExpiration = 3600;
+        if(mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()){
+            cacheExpiration = 0;
+        }
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("TAG","On success fetching config");
+                        mFirebaseRemoteConfig.activateFetched();
+                        applyReceivedLengthLimit();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("TAG","On failure fetching config");
+                    }
+                });
+    }
+
+    private void applyReceivedLengthLimit() {
+        Log.d("TAG",mFirebaseRemoteConfig.getDouble("latest_version")+"");
     }
 }
